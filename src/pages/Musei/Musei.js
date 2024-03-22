5adfe4b5c478d7ab424dbab2dfcf0b1373ed8766
Maksim <144213292+GridnevM@users.noreply.github.com>
2024-03-22 22:25:59 +0300

    Initialize project using Create React App

--- src/pages/Musei/Musei.js
+++ src/pages/Musei/Musei.js
@@ -1,10 +1,15 @@
+import main_page from "./../../img/main_page.png"
+import "./style.css"
 const Musei = () => {
     return ( 
         <div className="Musei">
          
          <div className="r1">
             <div className="photo1">
-                
+                <img src="main_page" alt=""/>
+            </div>
+            <div className="text1">
+               <p>Исторический музей</p>
             </div>
          </div>
     
