523f094fdd0655de234f7f64881258223f0e9b8b
Bogdan_Ageew <90950034+EdwardEqwee@users.noreply.github.com>
2024-04-10 10:31:07 +0300

    1

--- src/pages/Dendrik/Dendrik.js
+++
@@ -1,9 +0,0 @@
-const Dendrik = () => {
-    return ( 
-        <>
-        
-        </>
-     );
-}
- 
-export default Dendrik;
--- src/pages/Musei/Musei.js
+++
@@ -1,9 +0,0 @@
-const Musei = () => {
-    return ( 
-        <>
-        
-        </>
-     );
-}
- 
-export default Musei;
