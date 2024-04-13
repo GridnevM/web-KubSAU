30b870a1c50d242df800b58ef769b216cd2659db
Bogdan_Ageew <cat.edward145@gmail.com>
2024-04-13 19:21:29 +0300

    Адаптив главной страницы , вверхва и низа

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,40 +1,20 @@
+import React from 'react';
 import logo from "./../../img/logo-white.png";
 import "./style.css";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return ( 
-       <div className="Header">
-            <div className="menu">
-                <ul className="menu-list">
-                    <li className="menu-university">
-                        <NavLink to="/University" className="menu_link">
-                           <p>УНИВЕРСИТЕТ</p> 
-                        </NavLink>
-                    </li>
-                    <li className="menu-korpusa">
-                        <NavLink to ="/Korpusa" className="menu_link">
-                        <p>КОРПУСА</p>
-                        </NavLink>
-                    </li>
-                    <li >
-                    <NavLink to="/" className="logo">
-                    <img src={logo} alt="logo"/>
-                    </NavLink>
-                    </li>
-                    <li className="menu-musei">
-                        <NavLink to="/Musei" className="menu_link">
-                        <p>МУЗЕИ</p>
-                        </NavLink>
-
-                    </li>
-                    <li className="menu-garden">
-                        <NavLink to="/Dendrik" className="menu_link">
-                        <p>ДЕНДРАРИЙ</p>
-                        </NavLink>
-                    </li>
-                </ul>
-            </div>
+    return (
+        <div className="Header">
+            <NavLink to="/" className="logo">
+                <img src={logo} alt="logo" />
+            </NavLink>
+            <ul className="menu-list">
+                <li><NavLink to="/University" className="menu_link">УНИВЕРСИТЕТ</NavLink></li>
+                <li><NavLink to="/Korpusa" className="menu_link">КОРПУСА</NavLink></li>
+                <li><NavLink to="/Musei" className="menu_link">МУЗЕИ</NavLink></li>
+                <li><NavLink to="/Dendrik" className="menu_link">ДЕНДРАРИЙ</NavLink></li>
+            </ul>
         </div>
     );
 }
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,54 +1,35 @@
-import "./style.css"
-import main_page from "./../../img/main_page.png"
-import main_page2 from "./../../img/main_page2.png"
-import main_page3 from"./../../img/main_page3.png";
-import main_page4 from"./../../img/main_page4.png";
-import main_page5 from"./../../img/main_page5.png";
-import main_page6 from"./../../img/main_page6.png";
-import main_page7 from"./../../img/main_page7.png";
+import React from 'react';
+import "./style.css";
 import { NavLink } from "react-router-dom";
+import main_page from "./../../img/main_page.png";
+import main_page2 from "./../../img/main_page2.png";
+import main_page3 from "./../../img/main_page3.png";
+import main_page4 from "./../../img/main_page4.png";
+import main_page5 from "./../../img/main_page5.png";
+import main_page6 from "./../../img/main_page6.png";
+import main_page7 from "./../../img/main_page7.png";
+
 const Main = () => {
-    return (  
-          <div className="Main">        
-            <div className="text">
-                <p>Путеводитель по интересным местам КубГАУ</p>
+    return (
+        <div className="Main">
+            <div className="text">Путеводитель по интересным местам КубГАУ</div>
+            <div className="Photo">
+                <img src={main_page} alt="1" className="photo"/>
+                <img src={main_page2} alt="2" className="photo"/>
+                <img src={main_page3} alt="3" className="photo"/>
+                <img src={main_page4} alt="4" className="photo"/>
+                <img src={main_page5} alt="5" className="photo"/>
+                <img src={main_page6} alt="6" className="photo"/>
+                <img src={main_page7} alt="7" className="photo"/>
             </div>
-           
-                  <div className="Photo">
-                <div className="photo photo1">
-                  <img src={main_page} alt="1"/>
-                </div>
-                <div className="photo photo2">
-                  <img src={main_page2} alt="2"/>
-                </div>
-                <div className="photo photo3">
-                  <img src={main_page3} alt="3"/>
-                </div>
-                <div className="photo photo7">
-                  <img src={main_page7} alt="7"/>
-                </div>
-                <div className="photo photo4">
-                  <img src={main_page4} alt="4"/>
-                </div>
-                <div className="photo photo5">
-                  <img src={main_page5} alt="5"/>
-                </div>
-                <div className="photo photo6">
-                  <img src={main_page6} alt="6"/>
-                </div>
-              </div>
-              <div className="text1">
+            <div className="text1">
                 <p>Кубанский государственный аграрный университет - вуз с богатой историей и просторной территорией, на территории 174га расположилось  учебных корпусов, спортивный комплекс с бассейном и скалодромом, а также большое количество..... dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               </div>
-                <NavLink to="/University">
-                <button className="button">
-                  Увидеть больше!
-                  </button>
-                  </NavLink>
-            </div>    
-   
-        
+            <NavLink to="/University">
+                <button className="button">Увидеть больше!</button>
+            </NavLink>
+        </div>
     );
 }
- 
+
 export default Main;
