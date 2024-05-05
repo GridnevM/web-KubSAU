da54c873d9c4e569f4ab03151efe2344f1d83f21
Bogdan_Ageew <cat.edward145@gmail.com>
2024-05-05 21:13:13 +0300

    Страница с корпусами версия 1

--- django_project/react_application/src/pages/Korpusa/Korpus.js
+++ django_project/react_application/src/pages/Korpusa/Korpus.js
@@ -1,9 +1,106 @@
+import React from 'react';
+import './korpus.css'; // Ensure your CSS file is linked correctly
+
 const Korpus = () => {
-    return (  
-        <>
-        
-        </>
+    return (
+        <div className="main-content">
+            <h1 className="main-title">Учебные корпуса Кубанского ГАУ</h1>
+            <p className="main-description">На территории университета расположено много учебных корпусов с аудиториями с современным оборудованием, а пока снова пару слов на латыни... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div className="courses">
+
+            <div className="course">
+                    <div className="photo-placeholder"></div>
+                    <div className="course-info">
+                        <h2>Главный учебный корпус (ГЛ)</h2>
+                        <p>Старейший учебный корпус, здесь проходят занятия у таких-то факультетов, а также располагается крутейшая лаборатория для животноводов</p>
+                        <button>Подробнее →</button>
+                    </div>
+                </div>
+
+                <div className="course">
+                    <div className="photo-placeholder"></div>
+                    <div className="course-info">
+                        <h2>Зооинженерный корпус (ЗОО)</h2>
+                        <p>Старейший учебный корпус, здесь проходят занятия таких-то факультетов, а также располагается крупнейшая лаборатория для животноводов</p>
+                        <button>Подробнее →</button>
+                    </div>
+                </div>
+
+                <div className="course">
+                    <div className="photo-placeholder"></div>
+                    <div className="course-info">
+                        <h2>Корпус ветеринарного отделения (ВМ)</h2>
+                        <p>Старейший учебный корпус, здесь проходят занятия у таких-то факультетов, а также располагается крутейшая лаборатория для животноводов</p>
+                        <button>Подробнее →</button>
+                    </div>
+                </div>
+
+                <div className="course">
+                    <div className="photo-placeholder"></div>
+                    <div className="course-info">
+                        <h2>Корпус электрификации (ЭЛ)</h2>
+                        <p>Старейший учебный корпус, здесь проходят занятия у таких-то факультетов, а также располагается крутейшая лаборатория для животноводов</p>
+                        <button>Подробнее →</button>
+                    </div>
+                </div>
+
+                <div className="course">
+                    <div className="photo-placeholder"></div>
+                    <div className="course-info">
+                        <h2>Корпус физвоспитания (СОЦ)</h2>
+                        <p>Старейший учебный корпус, здесь проходят занятия таких-то факультетов, а также располагается крупнейшая лаборатория для животноводов</p>
+                        <button>Подробнее →</button>
+                    </div>
+                </div>
+
+                <div className="course">
+                    <div className="photo-placeholder"></div>
+                    <div className="course-info">
+                        <h2>Корпус гидромелиорации (ГД)</h2>
+                        <p>Старейший учебный корпус, здесь проходят занятия у таких-то факультетов, а также располагается крутейшая лаборатория для животноводов</p>
+                        <button>Подробнее →</button>
+                    </div>
+                </div>
+
+                <div className="course">
+                    <div className="photo-placeholder"></div>
+                    <div className="course-info">
+                        <h2>Корпус  защиты растений (ЗР)</h2>
+                        <p>Старейший учебный корпус, здесь проходят занятия таких-то факультетов, а также располагается крупнейшая лаборатория для животноводов</p>
+                        <button>Подробнее →</button>
+                    </div>
+                </div>
+
+                 защиты растений<div className="course">
+                    <div className="photo-placeholder"></div>
+                    <div className="course-info">
+                        <h2>Корпус  механизации (МХ)</h2>
+                        <p>Старейший учебный корпус, здесь проходят занятия у таких-то факультетов, а также располагается крутейшая лаборатория для животноводов</p>
+                        <button>Подробнее →</button>
+                    </div>
+                </div>
+
+                <div className="course">
+                    <div className="photo-placeholder"></div>
+                    <div className="course-info">
+                        <h2>Корпус экономического отделения (ЭК)</h2>
+                        <p>Старейший учебный корпус, здесь проходят занятия у таких-то факультетов, а также располагается крутейшая лаборатория для животноводов</p>
+                        <button>Подробнее →</button>
+                    </div>
+                </div>
+
+                <div className="course">
+                    <div className="photo-placeholder"></div>
+                    <div className="course-info">
+                        <h2>Корпус заочного обучения (НОТ)</h2>
+                        <p>Старейший учебный корпус, здесь проходят занятия у таких-то факультетов, а также располагается крутейшая лаборатория для животноводов</p>
+                        <button>Подробнее →</button>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
     );
 }
- 
+
 export default Korpus;
