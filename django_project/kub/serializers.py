1c2cddf6646f0e9d50e76896b6c00229297c349c
Maksim <144213292+GridnevM@users.noreply.github.com>
2024-05-31 14:41:35 +0300

    Delete django_project directory

--- django_project/kub/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import kub
-
-class StudentSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = kub 
-        fields = ('text')
